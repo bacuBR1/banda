3f10815dda024e576908e408cbf5bba857f13092
bacuBR1 <lucasmlunag@gmail.com>
2026-03-26 21:26:28 -0300

    Add files via upload

---
+++ back.js
@@ -0,0 +1,36 @@
+var abrir = false;
+
+function mude() {
+    const box = document.getElementById("menu-hiden");
+        abrir = !abrir;
+
+    if (abrir === true) {
+        box.style.display = "block";
+    } else {
+        box.style.display = "none";
+    }
+}
+
+function album1() {
+    window.location.href = ""
+}
+
+function album1_alt() {
+    window.location.href = ""
+}
+
+function album2() {
+    window.location.href = ""
+}
+
+function compra_album1() {
+    window.location.href = ""
+}
+
+function compra_album1_2() {
+    window.location.href = ""
+}
+
+function compra_album2() {
+    window.location.href = ""
+}
